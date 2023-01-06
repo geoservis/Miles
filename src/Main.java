a93e243b67ca7f3c83cf639ae86779b87d3581aa
Svetlana Rumyantseva <geoservis2010@bk.ru>
2023-01-06 22:31:00 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+           int price = 400;
+           int miles = price/20;
+
+           System.out.println("Бонусные милли:" + miles);
+
+    }
+}
